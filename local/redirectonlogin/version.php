c00eb705317cad237b846931fcc782f1f9d12e95
korenerok <marvin.bernal@gmx.com>
2022-05-02 16:09:30 -0400

    Changed version number

--- local/redirectonlogin/version.php
+++ local/redirectonlogin/version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_redirectonlogin'; 
-$plugin->version = 2022050200;
+$plugin->version = 2022050201;
 $plugin->requires = 2019111200;
