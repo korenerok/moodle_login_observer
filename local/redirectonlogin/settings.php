86c07b4cc72715fc635211d8ba0fdb16883225f5
korenerok <marvin.bernal@gmx.com>
2022-05-03 10:52:49 -0400

    Added settings correctly

--- local/redirectonlogin/settings.php
+++ local/redirectonlogin/settings.php
@@ -1,88 +1,53 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-use local_eabcsocial\utils;
+/**
+ * Plugin administration pages are defined here.
+ *
+ * @package     local_redirectonlogin
+ * @category    local
+ * @copyright   2022 @korenerok <marvin.bernal@gmx.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-require_once('lib.php');
+
 defined('MOODLE_INTERNAL') || die;
+
 if ($hassiteconfig) { // needs this condition or there is error on login page
     global $DB;
 
-    $title = get_string('pluginname', 'local_eabcsocial');
-    $settings = new admin_settingpage('local_eabcsocial', $title);
+    $title = get_string('pluginname', 'local_redirectonlogin');
+    $settings = new admin_settingpage('local_redirectonlogin', $title);
 
-    $displaylist = core_course_category::make_categories_list('moodle/course:create');
+    $displaylist=get_courses();
 
-    $name = 'local_eabcsocial/categoryid';
-    $description = get_string('desc_category', 'local_eabcsocial');
-    $title = get_string('title_category', 'local_eabcsocial');
+    $name = 'local_redirectonlogin/home_course_student';
+    $description = get_string('home_course_student_desc', 'local_redirectonlogin');
+    $title = get_string('home_course_student', 'local_redirectonlogin');
     $settings->add(new admin_setting_configselect($name, $title, $description, key($displaylist), $displaylist));
 
-    // $name = 'local_eabcsocial/category_self';
-    // $description = get_string('desc_category_self', 'local_eabcsocial');
-    // $title = get_string('title_category_self', 'local_eabcsocial');
-    // $settings->add(new admin_setting_configselect($name, $title, $description, key($displaylist), $displaylist));
+    $name = 'local_redirectonlogin/home_course_teacher_or_staff';
+    $description = get_string('home_course_teacher_or_staff_desc', 'local_redirectonlogin');
+    $title = get_string('home_course_teacher_or_staff', 'local_redirectonlogin');
+    $settings->add(new admin_setting_configselect($name, $title, $description, key($displaylist), $displaylist));
 
-    // $name = 'local_eabcsocial/category_manual';
-    // $description = get_string('desc_category_manual', 'local_eabcsocial');
-    // $title = get_string('title_category_manual', 'local_eabcsocial');
-    // $settings->add(new admin_setting_configselect($name, $title, $description, key($displaylist), $displaylist));
+    $name = 'local_redirectonlogin/home_course_parent';
+    $description = get_string('home_course_parent_desc', 'local_redirectonlogin');
+    $title = get_string('home_course_parent', 'local_redirectonlogin');
+    $settings->add(new admin_setting_configselect($name, $title, $description, key($displaylist), $displaylist));
 
-    $name = 'local_eabcsocial/limite_vistos';
-    $description = get_string('desc_limite_vistos', 'local_eabcsocial');
-    $title = get_string('title_limite_vistos', 'local_eabcsocial');
-    $settings->add(new admin_setting_configtext($name, $title, $description, 5, PARAM_INT));
-
-    $name = 'local_eabcsocial/message_enrol';
-    $description = get_string('desc_message_enrol', 'local_eabcsocial');
-    $title = get_string('title_message_enrol', 'local_eabcsocial');
-    $settings->add(new admin_setting_configtext($name, $title, $description, 'NO SE PUEDE MATRICULAR EN ESTE CURSO', PARAM_TEXT));
-
-    $categoryid = get_config('local_eabcsocial', 'categoryid');
-    $array_cat = utils::get_list_valid_category($categoryid);
-
-    $arraycats = [];
-    foreach ($array_cat as $cat) {
-        if ($cat == 1) {
-            continue;
-        }
-        $category = $DB->get_record('course_categories', ['id' => $cat]);
-
-        $arraycats[$cat] = $category->name;
-    }
-
-    foreach ($arraycats as $key => $catname) {
-        $fileid = 'cat'.$key;
-        $name = 'local_eabcsocial/catimage'.$key;
-        $title = get_string('catimage', 'local_eabcsocial', $catname);
-        $description = get_string('catimagedesc', 'local_eabcsocial', $catname);
-        $setting = new admin_setting_configstoredfile($name, $title, $description, $fileid);
-        $settings->add($setting);
-    }
-/*
-    $name = 'local_eabcsocial/show_vigentes';
-    $description = "Mostrar la sección de cursos Vigentes";
-    $title = "Mostrar los cursos vigentes";
-    $settings->add(new admin_setting_configcheckbox($name, $title, $description, 0));
-
-    $name = 'local_eabcsocial/show_masvistos';
-    $description = "Mostrar la sección de cursos más vistos";
-    $title = "Mostrar los cursos más vistos";
-    $settings->add(new admin_setting_configcheckbox($name, $title, $description, 0));
-
-    $name = 'local_eabcsocial/show_sugeridos';
-    $description = "Mostrar la sección de cursos sugeridos por vos";
-    $title = "Mostrar los cursos sugeridos";
-    $settings->add(new admin_setting_configcheckbox($name, $title, $description, 0));
-
-    $name = 'local_eabcsocial/show_asignados';
-    $description = "Mostrar la sección de cursos asignados";
-    $title = "Mostrar los cursos asignados";
-    $settings->add(new admin_setting_configcheckbox($name, $title, $description, 0));
-
-    $name = 'local_eabcsocial/show_loquebuscas';
-    $description = "Mostrar la sección que contiene: mis cursos, cursos por competencias, por categorías y por temática";
-    $title = "Mostrar cursos por varios criterios";
-    $settings->add(new admin_setting_configcheckbox($name, $title, $description, 0));
-*/
     $ADMIN->add('localplugins', $settings);
 }
