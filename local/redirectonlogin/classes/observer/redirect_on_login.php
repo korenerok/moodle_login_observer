7493dad8950878abe937861bd940dd37be3aeeb4
korenerok <marvin.bernal@gmx.com>
2022-05-03 10:16:52 -0400

    Added classify user, with several conditions.
    Used regular expressions to match email address format desired.

--- local/redirectonlogin/classes/observer/redirect_on_login.php
+++ local/redirectonlogin/classes/observer/redirect_on_login.php
@@ -16,4 +16,32 @@ class redirect_on_login {
         redirect(new moodle_url('/course/view.php',['id'=>$course_id,'email'=>$user->email]));
     }
 
+    protected static function classify_user($user){
+        $id_teacher_or_staff=2;
+        $id_student=3;
+        $id_parent=4;
+        if($user->auth=='manual'){
+            return $id_parent;
+        }
+        if($domain=explode('@',$user->email)){
+            $username=$domain[0];
+            $domain=$domain[1];
+        }
+        if($domain=='itec.pr' || $domain=='de.pr.gov'){
+            return $id_teacher_or_staff;
+        }
+        if(substr($domain,-3)==".pr"){
+            $match=[];
+            if(preg_match("/de([0-9]+)/",$username,$match)===1 ){
+                if($username===$match[0])
+                    return $id_teacher_or_staff;
+            }
+            $match=[];
+            if(preg_match("/e([0-9]+)/",$username,$match)===1 ){
+                if($username===$match[0])
+                    return $id_student;
+            }
+        }
+        return 1;
+    }
 }
