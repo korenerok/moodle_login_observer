72107db4d9791c6f5bb55825f9629d60716b4cf2
korenerok <marvin.bernal@gmx.com>
2022-05-03 10:15:44 -0400

    Added redirect user function to observer

--- local/redirectonlogin/classes/observer/redirect_on_login.php
+++ local/redirectonlogin/classes/observer/redirect_on_login.php
@@ -9,8 +9,11 @@ require_once($CFG->dirroot.'/user/lib.php');
 class redirect_on_login {
     public static function redirect_user(\core\event\user_loggedin $event) {
         $user=current(user_get_users_by_id([$event->userid]));
-        
-        if(count(get_courses())>1)
-            redirect(new moodle_url('/course/view.php',['id'=>2,'email'=>$user->email]));
+        $course_id=1;
+        if(count(get_courses())>1){
+            $course_id=self::classify_user($user);
+        }
+        redirect(new moodle_url('/course/view.php',['id'=>$course_id,'email'=>$user->email]));
     }
+
 }
